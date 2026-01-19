b44dd32215e8edb26b3165940cc531e19f75c0fe
odynets-uk <the.odynets@gmail.com>
2026-01-19 05:20:18 +0200

    Update turso-laravel.php

--- config/turso-laravel.php
+++ config/turso-laravel.php
@@ -4,16 +4,15 @@
 return [
     'client' => [
         'connect_timeout' => env('TURSO_CONNECT_TIMEOUT', 2),
-        'timeout'         => env('TURSO_REQUEST_TIMEOUT', 5),
+        'timeout' => env('TURSO_REQUEST_TIMEOUT', 5),
     ],
 
     'sync_command' => [
-        'node_path'       => env('NODE_PATH'), // Full path to the node executable. E.g: /usr/bin/node
+        'node_path' => env('NODE_PATH'), // Full path to the node executable. E.g: /usr/bin/node
         'script_filename' => 'turso-sync.mjs',
-        'script_path'     => realpath(__DIR__ . '/..'),
-        'timeout'         => 60,
-    ],'
-
-    // Added
-    'auto_sync_on_terminate' => env('TURSO_AUTO_SYNC', true),
+        'script_path' => realpath(__DIR__.'/..'),
+        'timeout' => 60,
+    ],
+    
+    'auto_sync_on_terminate' => env('TURSO_AUTO_SYNC', false), // Added
 ];
