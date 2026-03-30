b95ce29e83933bf45ad4ca5865a11a5ebc9d79fd
odynets-uk <the.odynets@gmail.com>
2026-03-30 05:32:28 +0300

    Update TursoPDOStatement.php
    restored public function setFetchMode(int $mode, mixed ...$args): bool

--- src/Database/TursoPDOStatement.php
+++ src/Database/TursoPDOStatement.php
@@ -34,7 +34,7 @@ class TursoPDOStatement extends PDOStatement
         protected array $options = [],
     ) {}
 
-    public function setFetchMode(int $mode, mixed ...$args): true
+    public function setFetchMode(int $mode, mixed ...$args): bool
     {
         $this->fetchMode = $mode;
 
